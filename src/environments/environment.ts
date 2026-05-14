85de89ab590fee89553e0b78532c367bdbd8cae7
mohammadfakih02 <mohammadfakih025@gmail.com>
2026-05-14 14:42:20 +0300

    fix: addressed bug with infinetly looping routing in auth service and fixed backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5057',
+  apiUrl: 'http://localhost:5057/api',
   signalRUrl: 'http://localhost:5057/hubs',
   liveKitUrl: 'ws://localhost:7880'
 };
